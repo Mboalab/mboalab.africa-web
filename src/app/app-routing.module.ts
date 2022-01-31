69aa4e5cfee377362801f7543b2d0983f7a2dce7
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-01-31 19:34:02 +0500

    feat: add blank pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { BiotechComponent } from './pages/biotech/biotech.component';
+import { OpenHealthComponent } from './pages/open-health/open-health.component';
+import { KnowledgeHubComponent } from './pages/knowledge-hub/knowledge-hub.component';
+import { NewsAndEventsComponent } from './pages/news-and-events/news-and-events.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'biotech', component: BiotechComponent },
+  { path: 'open-health', component: OpenHealthComponent },
+  { path: 'knowledge-hub', component: KnowledgeHubComponent },
+  { path: 'news-and-events', component: NewsAndEventsComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
 ];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
     { name: 'About', link: 'about', children: [{name: 'Who we are', link: 'who-we-are'}, {name: 'Team', link: 'team'}, {name: 'Partners', link:'partners'}]},
     { name: 'Biotech', link: 'biotech', children: [{name: 'Bio manufacturing and Research' ,link: 'bio-manu-and-research'}, {name: 'Research training hub and Exchange hub', link: 'hubs'}, {name: 'Projects',link: 'biotech-projects'}, {name: 'Beneficial Bio',link: 'beneficial-bio'}]},
     { name: 'Open Health', link: 'open-health', children: [{name: 'Projects', link: 'openhealth-projects'}, {name: 'Diagnose',link: 'diagnose'}, {name: 'AI for Health', link: 'ai-for-health'}]},
-    { name: 'Knowledge Health', link:'knowledge-health', children: [{name: 'Open Educational Resources', link:'edu-resources'}, {name: 'Talks & Conferences',link: 'talks-and-confs'}]},
+    { name: 'Knowledge Hub', link:'knowledge-hub', children: [{name: 'Open Educational Resources', link:'edu-resources'}, {name: 'Talks & Conferences',link: 'talks-and-confs'}]},
     { name: 'News & Events', link: 'news-and-events', children: [{name: 'Blog', link: 'blog'}, {name: 'Opportunities', link: 'opportunities'}]},
     { name: 'Contact', link: 'contact' }
   ];
